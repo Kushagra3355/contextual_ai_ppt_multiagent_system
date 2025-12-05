8b1cc9b0b4f78b8082d9b3b00ddd2eb917676e67
Kushagra3355 <kushagra.omar@gmail.com>
2025-12-05 19:07:59 +0530

    testing API functions

--- test.py
+++ test.py
@@ -1,1 +1,9 @@
-print("hello")
+from langchain_openai import ChatOpenAI
+from dotenv import load_dotenv
+
+load_dotenv()
+
+model = ChatOpenAI(model = "gpt-3.5-turbo")
+
+result = model.invoke("Write a joke on  cricket")
+print(result.content)
