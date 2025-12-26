625d71fb006e590b433788306da340ba37ebd4d6
Kushagra3355 <kushagra.omar@gmail.com>
2025-12-26 13:46:12 +0530

    schemas

--- test.py
+++
@@ -1,9 +0,0 @@
-from langchain_openai import ChatOpenAI
-from dotenv import load_dotenv
-
-load_dotenv()
-
-model = ChatOpenAI(model = "gpt-3.5-turbo")
-
-result = model.invoke("Write a joke on  cricket")
-print(result.content)
