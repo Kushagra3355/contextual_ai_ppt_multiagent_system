0a83b342e6d3abe1aefa0e3899b9bffeeed16e47
Kushagra3355 <kushagra.omar@gmail.com>
2026-01-01 02:26:17 +0530

    rag dependencies for global access

--- app/dependencies.py
+++ app/dependencies.py
@@ -2,6 +2,8 @@ from langchain_openai import ChatOpenAI
 from langchain_openai import OpenAIEmbeddings
 from app.config import MODEL_NAME, EMBED_MODEL_NAME, TEMPERATURE, DIMENSIONS, CHUNK_SIZE
 
+_rag_pipeline = None
+
 
 def llm():
     """Initialized the Chat Model"""
@@ -13,3 +15,13 @@ def embed_model():
     return OpenAIEmbeddings(
         model=EMBED_MODEL_NAME, dimensions=DIMENSIONS, chunk_size=CHUNK_SIZE
     )
+
+
+def get_rag_pipeline():
+    from rag_pipeline.pipeline import RAGPipeline
+
+    global _rag_pipeline
+    if _rag_pipeline is None:
+        _rag_pipeline = RAGPipeline()
+        _rag_pipeline.load()
+    return _rag_pipeline
